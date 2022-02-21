fc92a186364e2d77acd2320fe01e156b0cde0d07
Kohulan <kohulan.rajan@uni-jena.de>
2022-02-21 12:20:01 +0100

    Repacking model checkpoints as model

---
+++ DECIMER_V2/src/Predictor_exported.py
@@ -0,0 +1,85 @@
+import argparse
+import config
+import os, sys, logging
+import pickle
+import tensorflow as tf
+import time
+
+# Silence tensorflow model loading warnings.
+logging.getLogger('absl').setLevel('ERROR')
+
+# Silence tensorflow errors. optional not recommened if your model is not working properly.
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+print(tf.__version__)
+
+# Set the absolute path
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+# Set model to run on default GPU and allow memory to grow as much as needed.
+# This allows us to run multiple instances of inference in the same GPU.
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# Load important pickle files which consists the tokenizers and the maxlength setting
+tokenizer = pickle.load(open(os.path.join(HERE,"DECIMER_Packed_model/assets/tokenizer_Isomeric_SMILES.pkl"), "rb"))
+
+def main():
+    """
+    This function take the path of the image as user input
+    and returns the predicted SMILES as output in CLI.
+    
+    Agrs:
+        str: image_path
+
+    Returns:
+        str: predicted SMILES
+
+    """
+    if len(sys.argv) != 2:
+        print("Usage: {} $image_path".format(sys.argv[0]))
+    else:
+        SMILES = predict_SMILES(sys.argv[1])
+        print(SMILES)
+
+def detokenize_output(predicted_array: int) -> str:
+    """
+    This function takes the predited tokens from the DECIMER model
+    and returns the decoded SMILES string.
+
+    Args:
+        predicted_array (int): Predicted tokens from DECIMER
+
+    Returns:
+        (str): SMILES representation of the molecule
+    """
+    outputs = [tokenizer.index_word[i] for i in predicted_array[0].numpy()]
+    prediction = ''.join([str(elem) for elem in outputs]).replace("<start>","").replace("<end>","")
+    
+    return prediction
+
+# Load DECIMER model_packed
+DECIMER_V2 = tf.saved_model.load(os.path.join(HERE,'DECIMER_Packed_model'))
+
+def predict_SMILES(image_path: str) -> str:
+    """
+    This function takes an image path (str) and returns the SMILES 
+    representation of the depicted molecule (str).
+
+    Args:
+        image_path (str): Path of chemical structure depiction image
+
+    Returns:
+        (str): SMILES representation of the molecule in the input image
+    """
+    chemical_structure = config.decode_image(image_path)
+    predicted_tokens = DECIMER_V2(chemical_structure)
+    predicted_SMILES = detokenize_output(predicted_tokens)
+
+
+    return predicted_SMILES
+
+
+if __name__ == "__main__":
+    main()
---
+++ DECIMER_V2/src/Repack_model.py
@@ -0,0 +1,160 @@
+import os
+import sys
+import tensorflow as tf
+
+import pickle
+from PIL import Image
+from selfies import decoder
+import Transformer_decoder
+import Efficient_Net_encoder
+import config
+import efficientnet.tfkeras as efn
+print(tf.__version__)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+# Set GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+	tf.config.experimental.set_memory_growth(gpu, True)
+
+# load assets
+tokenizer = pickle.load(open("tokenizer_Isomeric_SMILES.pkl", "rb"))
+max_length = pickle.load(open("max_length_Isomeric_SMILES.pkl", "rb"))
+
+# Image parameters
+IMG_EMB_DIM = (10, 10, 232)
+IMG_EMB_DIM = (IMG_EMB_DIM[0] * IMG_EMB_DIM[1], IMG_EMB_DIM[2])
+IMG_SHAPE = (299, 299, 3)
+PE_INPUT = IMG_EMB_DIM[0]
+IMG_SEQ_LEN, IMG_EMB_DEPTH = IMG_EMB_DIM
+D_MODEL = IMG_EMB_DEPTH
+
+# Network parameters
+N_LAYERS = 4
+D_MODEL = 512
+D_FF = 2048
+N_HEADS = 8
+DROPOUT_RATE = 0.1
+
+# Misc
+MAX_LEN = max_length
+VOCAB_LEN = len(tokenizer.word_index)
+PE_OUTPUT = MAX_LEN
+TARGET_V_SIZE = VOCAB_LEN
+REPLICA_BATCH_SIZE = 1
+
+# Config Encoder
+PREPROCESSING_FN = tf.keras.applications.efficientnet.preprocess_input
+BB_FN = Efficient_Net_encoder.get_efficientnetv2_backbone
+
+# Config Model
+testing_config = config.Config()
+
+testing_config.initialize_encoder_config(
+	image_embedding_dim=IMG_EMB_DIM,
+	preprocessing_fn=PREPROCESSING_FN,
+	backbone_fn=BB_FN,
+	image_shape=IMG_SHAPE,
+	do_permute=IMG_EMB_DIM[1] < IMG_EMB_DIM[0],
+)
+
+testing_config.initialize_transformer_config(
+	vocab_len=VOCAB_LEN,
+	max_len=MAX_LEN,
+	n_transformer_layers=N_LAYERS,
+	transformer_d_dff=D_FF,
+	transformer_n_heads=N_HEADS,
+	image_embedding_dim=IMG_EMB_DIM,
+)
+
+# Prepare model
+optimizer, encoder, transformer = config.prepare_models(
+	encoder_config=testing_config.encoder_config,
+	transformer_config=testing_config.transformer_config,
+	replica_batch_size=REPLICA_BATCH_SIZE,
+	verbose=0,
+)
+
+# Load trained model checkpoint from directory
+checkpoint_path ="checkpoints_SMILES_TPU32"
+ckpt = tf.train.Checkpoint(
+	encoder=encoder, transformer=transformer, optimizer=optimizer
+)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=50)
+if ckpt_manager.latest_checkpoint:
+	ckpt.restore(tf.train.latest_checkpoint(checkpoint_path))
+	start_epoch = int(ckpt_manager.latest_checkpoint.split("-")[-1])
+
+class DECIMER_Predictor(tf.Module):
+
+	def __init__(self, encoder, tokenizer, transformer, max_length):
+		self.encoder = encoder
+		self.tokenizer = tokenizer
+		self.transformer = transformer
+		self.max_length = max_length
+		
+	def __call__(self, Decoded_image):
+		assert isinstance(Decoded_image, tf.Tensor)
+		if len(Decoded_image.shape) == 0:
+			sentence = Decoded_image[tf.newaxis]
+			
+		_image_batch = tf.expand_dims(Decoded_image, 0)
+		_image_embedding = encoder(_image_batch, training=False)
+
+
+		start_token = tf.cast(tf.convert_to_tensor([tokenizer.word_index["<start>"]]),tf.int32)
+		end_token = tf.cast(tf.convert_to_tensor([tokenizer.word_index["<end>"]]),tf.int32)
+		
+		output_array = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+		output_array = output_array.write(0, start_token)
+
+		for t in tf.range(max_length):
+			output = tf.transpose(output_array.stack())
+			combined_mask = Transformer_decoder.create_mask(None, output)
+
+			# predictions.shape == (batch_size, seq_len, vocab_size)
+			prediction_batch, _ = transformer(
+				_image_embedding, output, training=False, look_ahead_mask=combined_mask)
+
+			# select the last word from the seq_len dimension
+			predictions = prediction_batch[:, -1:, :]  # (batch_size, 1, vocab_size)
+
+			predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+		
+			output_array = output_array.write(t+1, predicted_id[0])
+			
+			if predicted_id == end_token:
+				break
+		output = tf.transpose(output_array.stack())
+
+
+		return output
+
+def detokenize_output(predicted_array):
+    outputs = [tokenizer.index_word[i] for i in predicted_array[0].numpy()]
+    prediction = ''.join([str(elem) for elem in outputs]).replace("<start>","").replace("<end>","")
+    
+    return prediction
+
+DECIMER = DECIMER_Predictor(encoder, tokenizer, transformer, MAX_LEN)
+
+class ExportDECIMERPredictor(tf.Module):
+  def __init__(self, DECIMER):
+    self.DECIMER = DECIMER
+
+  @tf.function
+  def __call__(self, Decoded_Image):
+
+    result = self.DECIMER(Decoded_Image)
+
+    return result
+
+DECIMER = ExportDECIMERPredictor(DECIMER)
+
+tf.saved_model.save(DECIMER, export_dir='DECIMER_Packed_model')
+
+
+
