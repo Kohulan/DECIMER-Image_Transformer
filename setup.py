2024074731d40d1f11b7fe9a9952a04a3d69f4ce
Kohulan <kohulan.rajan@uni-jena.de>
2022-05-19 09:35:27 +0200

    TensorFlow version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="MIT",
     install_requires=[
-        "tensorflow>=2.7.1",
+        "tensorflow>=2.6.3",
         "opencv-python",
         "pystow",
         "efficientnet",
