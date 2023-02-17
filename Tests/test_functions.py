54dbbc8cb42800599b4058adade27a7d58a7d32a
Kohulan <kohulan.rajan@uni-jena.de>
2023-02-17 21:40:35 +0100

    fix: Test expected results

--- Tests/test_functions.py
+++ Tests/test_functions.py
@@ -3,6 +3,6 @@ from DECIMER import predict_SMILES
 
 def get_imagetosmiles():
 	img_path = 'Tests/caffeine.png'
-	expected_result = "CN1C=NC2=C1C(=O)N(C)C(=O)N2C"
+	expected_result = "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
 	actual_result = predict_SMILES(image_path)
 	assert expected_result == actual_result
