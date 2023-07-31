fb2fd2ac6c4cdb0120800d677c98e7c13e781a6f
Kohulan <kohulan.rajan@uni-jena.de>
2023-07-31 16:24:06 +0200

    Importing EfficientNet fixed

--- DECIMER/efficientnetv2/effnetv2_configs.py
+++ DECIMER/efficientnetv2/effnetv2_configs.py
@@ -15,7 +15,7 @@
 """EfficientNet V1 and V2 model configs."""
 import functools
 import re
-from efficientnetv2 import hparams
+from DECIMER.efficientnetv2 import hparams
 
 cfg_register = functools.partial(hparams.register, prefix="cfg:")
 
