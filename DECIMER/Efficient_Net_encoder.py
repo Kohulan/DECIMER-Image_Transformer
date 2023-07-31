7057fab3af87b7220b54424061378ebf884cd47c
Kohulan <kohulan.rajan@uni-jena.de>
2023-07-31 16:31:41 +0200

    fix: efficientNet Model params

--- DECIMER/Efficient_Net_encoder.py
+++ DECIMER/Efficient_Net_encoder.py
@@ -18,7 +18,7 @@ def get_efficientnetv2_backbone(
             "\n...At this time we only want to use the raw "
             "(no pretraining), headless, features with no pooling ...\n"
         )
-    backbone = efficientnetv2.effnetv2_model.EffNetV2Model(model_name=model_name)
+    backbone = effnetv2_model.EffNetV2Model(model_name=model_name)
     backbone(
         tf.ones((BATCH_SIZE_DEBUG, *input_shape)), training=False, features_only=True
     )
