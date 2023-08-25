491c7bc650b8fd879811a99e284bebfd7c497823
Otto Brinkhaus <brinkhaus@gmail.com>
2023-08-25 14:58:44 +0200

    add token-level confidence scores

--- DECIMER/Repack_model.py
+++ DECIMER/Repack_model.py
@@ -1,11 +1,10 @@
 import os
 import tensorflow as tf
-
+from typing import List, Tuple
 import pickle
 import DECIMER.Efficient_Net_encoder as Efficient_Net_encoder
 import DECIMER.Transformer_decoder as Transformer_decoder
 import DECIMER.config as config
-import DECIMER.utils as utils
 
 print(tf.__version__)
 
@@ -84,15 +83,50 @@ if ckpt_manager.latest_checkpoint:
     start_epoch = int(ckpt_manager.latest_checkpoint.split("-")[-1])
 
 
-def detokenize_output(predicted_array):
+def detokenize_output(
+    predicted_array: tf.Tensor
+) -> str:
+    """
+    This function takes the predicted array of tokens and returns the predicted SMILES
+    string.
+
+    Args:
+        predicted_array (tf.Tensor): Transformer Decoder output array (predicted tokens)
+
+    Returns:
+        str: SMILES string
+    """
     outputs = [tokenizer.index_word[i] for i in predicted_array[0].numpy()]
     prediction = (
         "".join([str(elem) for elem in outputs])
         .replace("<start>", "")
         .replace("<end>", "")
     )
-
     return prediction
+
+def detokenize_output_add_confidence(
+    predicted_array: tf.Tensor,
+    confidence_array: tf.Tensor,
+) -> List[Tuple[str, float]]:
+    """
+    This function takes the predicted array of tokens as well as the confidence values
+    returned by the Transformer Decoder and returns a list of tuples
+    that contain each token of the predicted SMILES string and the confidence
+    value.
+
+    Args:
+        predicted_array (tf.Tensor): Transformer Decoder output array (predicted tokens)
+
+    Returns:
+        str: SMILES string
+    """
+    prediction_with_confidence = [(tokenizer.index_word[predicted_array[0].numpy()[i]],
+                                   confidence_array[i].numpy())
+                                  for i in range(len(confidence_array))]
+    decoded_prediction_with_confidence = list([(utils.decoder(tok), conf) for tok, conf
+                                               in prediction_with_confidence[1:-1]])
+    decoded_prediction_with_confidence.append(prediction_with_confidence[-1])
+    return decoded_prediction_with_confidence
 
 
 class DECIMER_Predictor(tf.Module):
@@ -128,8 +162,6 @@ class DECIMER_Predictor(tf.Module):
             output (tf.Tensor[tf.int64]): predicted output as an array.
         """
         assert isinstance(Decoded_image, tf.Tensor)
-        if len(Decoded_image.shape) == 0:
-            sentence = Decoded_image[tf.newaxis]
 
         _image_batch = tf.expand_dims(Decoded_image, 0)
         _image_embedding = encoder(_image_batch, training=False)
@@ -143,6 +175,7 @@ class DECIMER_Predictor(tf.Module):
 
         output_array = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
         output_array = output_array.write(0, start_token)
+        confidence_array = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
 
         for t in tf.range(max_length):
             output = tf.transpose(output_array.stack())
@@ -155,16 +188,16 @@ class DECIMER_Predictor(tf.Module):
 
             # select the last word from the seq_len dimension
             predictions = prediction_batch[:, -1:, :]  # (batch_size, 1, vocab_size)
-
+            
             predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
+            confidence = predictions[-1][-1][int(predicted_id)]
             output_array = output_array.write(t + 1, predicted_id[0])
-
+            confidence_array = confidence_array.write(t + 1, confidence)
             if predicted_id == end_token:
                 break
         output = tf.transpose(output_array.stack())
 
-        return output
+        return output, confidence_array.stack()
 
 
 DECIMER = DECIMER_Predictor(encoder, tokenizer, transformer, MAX_LEN)
