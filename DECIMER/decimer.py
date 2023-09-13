f1f3e68d9589a7d85bf22705931ac9d9e3a06bca
Joao Damas CHST <joao.damas@syngenta.com>
2023-09-13 18:00:55 +0200

    pythonifies paths for model

--- DECIMER/decimer.py
+++ DECIMER/decimer.py
@@ -30,12 +30,12 @@ default_path = pystow.join("DECIMER-V2")
 
 # model download location
 model_url = "https://zenodo.org/record/8300489/files/models.zip"
-model_path = str(default_path) + "/DECIMER_model/"
+model_path = os.path.join(default_path.as_posix(), "DECIMER_model")
 
 # download models to a default location
 if (
     os.path.exists(model_path)
-    and os.stat(model_path + "/saved_model.pb").st_size != 28080309
+    and os.stat(os.path.join(model_path, "saved_model.pb")).st_size != 28080309
 ):
     shutil.rmtree(model_path)
     config.download_trained_weights(model_url, default_path)
@@ -43,10 +43,9 @@ elif not os.path.exists(model_path):
     config.download_trained_weights(model_url, default_path)
 
 # Load important pickle files which consists the tokenizers and the maxlength setting
-
 tokenizer = pickle.load(
     open(
-        default_path.as_posix() + "/DECIMER_model/assets/tokenizer_SMILES.pkl",
+        os.path.join(default_path.as_posix(), "DECIMER_model", "assets", "tokenizer_SMILES.pkl"),
         "rb",
     )
 )
