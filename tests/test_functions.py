1068c8ac92965c757ee94f561f9acef4cce61cee
Kohulan <kohulan.rajan@uni-jena.de>
2024-03-08 15:54:00 +0100

    fix: update tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,11 @@
 import pytest
 
 from DECIMER import predict_SMILES
-from DECIMER import predict_SMILES_with_confidence
 
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_imagetosmiles():
-    img_path = "Tests/caffeine.png"
+    img_path = "tests/caffeine.png"
     expected_result = "CN1C=NC2=C1C(=O)N(C)C(=O)N2C"
     actual_result = predict_SMILES(img_path)
     assert expected_result == actual_result
@@ -14,10 +13,18 @@ def test_imagetosmiles():
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_imagetosmilesWithConfidence():
-    img_path = "Tests/caffeine.png"
-    actual_result = predict_SMILES_with_confidence(img_path)
+    img_path = "tests/caffeine.png"
+    actual_result = predict_SMILES(img_path, confidence=True)
 
     for element, confidence in actual_result:
         assert (
             confidence >= 0.9
         ), f"Confidence for element '{element}' is below 0.9 (confidence: {confidence})"
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_imagetosmileshanddrawn():
+    img_path = "tests/caffeine.png"
+    expected_result = "CN1C=NC2=C1C(=O)N(C)C(=O)N2C"
+    actual_result = predict_SMILES(img_path, hand_drawn=True)
+    assert expected_result == actual_result
