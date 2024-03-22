8722aab16d12242858767107c7668fdc6903476c
Mathias Winkel <mathias.winkel@merckgroup.com>
2024-03-22 09:20:48 +0100

    restrict maximum tensorflow version to stay below 2.16.0
    otherwise tf.keras.callbacks.experimental is not available, see https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/experimental/BackupAndRestore

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import setuptools
 if (
     platform.processor() == "arm" or platform.processor() == "i386"
 ) and platform.system() == "Darwin":
-    tensorflow_os = "tensorflow-macos>=2.10.0"
+    tensorflow_os = "tensorflow-macos>=2.10.0,<=2.16.0"
 else:
-    tensorflow_os = "tensorflow>=2.12.0"
+    tensorflow_os = "tensorflow>=2.12.0,<=2.16.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
